ac90d6982a2aec1fea4679a5e98ff14294786dad
Raquel <raquealfaba@gmail.com>
2025-08-27 14:19:00 -0600

    fix(url): declaration from where is the catalog

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
         <div className="spacer" />
         <span className="badge">Valhora - Catálogo</span>
       </div>
-      <Flipbook src={file ? URL.createObjectURL(file) : '/Valhora.pdf'} />
+      <Flipbook src={file ? URL.createObjectURL(file) : "https://drive.google.com/uc?export=view&id=1N6G7p3QgGj9oiif_-XODlD4G4OadZ1On"} />
     </div>
   )
 }
