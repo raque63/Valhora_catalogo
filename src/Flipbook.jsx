3303aebe84c5a276c85c6ab85213eacf3e436399
Raquel <raquealfaba@gmail.com>
2025-08-27 12:23:08 -0600

    route(worker): use CDN

--- src/Flipbook.jsx
+++ src/Flipbook.jsx
@@ -4,9 +4,11 @@ import screenfull from "screenfull";
 import { Document, Page, pdfjs } from "react-pdf";
 import { Maximize, Minimize, ZoomIn, ZoomOut, ChevronLeft, ChevronRight, BookOpenCheck } from "lucide-react";
 
-import pdfWorker from "pdfjs-dist/build/pdf.worker.min?url";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
-pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
+// import pdfWorker from "pdfjs-dist/build/pdf.worker.min?url";
+
+// pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
 
 export default function Flipbook( { src } ) {
   const bookRef = useRef(null);
