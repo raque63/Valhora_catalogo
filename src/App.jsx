49fa75823a4400165d0a03facb97bc43893d92be
Raquel <raquealfaba@gmail.com>
2025-08-27 14:28:23 -0600

    feat(pdf): add new compressed pdf about valhora products

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Flipbook from './Flipbook'
 export default function App() {
   const [file, setFile] = useState(null)
 
-  const src = "https://drive.google.com/uc?export=view&id=1N6G7p3QgGj9oiif_-XODlD4G4OadZ1On"
 
   return (
     <div>
@@ -12,7 +11,7 @@ export default function App() {
         <div className="spacer" />
         <span className="badge">Valhora - Catálogo</span>
       </div>
-      <Flipbook src={file ? URL.createObjectURL(file) : "https://drive.google.com/uc?export=view&id=1N6G7p3QgGj9oiif_-XODlD4G4OadZ1On"} />
+      <Flipbook src={file ? URL.createObjectURL(file) : "/Valhora.pdf"} />
     </div>
   )
 }
